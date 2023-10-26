c24b10349bb81e55af623531f71dfe6cfde1bf85
ChanHeinKyaw <chanheinkyaw.mm@gmail.com>
2023-10-26 19:16:36 +0630

    product create form design

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -7,7 +7,63 @@
                 @include('layout.admin_sidebar')
             </div>
             <div class="col-md-8">
-                <h1>Product Create</h1>
+                <form class="table-bordered p-5" autocomplete="off">
+                    <h3 class="text-center text-info">Create Product</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" class="form-control rounded-0" id="name" name="name">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="price">Price</label>
+                                <input type="number" class="form-control rounded-0" id="price" name="price">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="cat_id">Category</label>
+                                <select name="cat_id" id="cat_id" class="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="sub_cat_id">Sub Category</label>
+                                <select name="sub_cat_id" id="sub_cat_id" class="form-control">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="file">File Input</label>
+                        <input type="file" class="form-control-file bg-secondary p-1 text-white" id="file" name="file">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description">Description</label>
+                        <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+                    </div>
+
+                    <div class="row d-flex justify-content-between no-gutters">
+                        <button class="btn btn-outline-secondary">Cancel</button>
+                        <button class="btn btn-outline-primary">Submit</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
